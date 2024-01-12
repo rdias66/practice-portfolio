b5dd9627189aa984df4750c5eb231c34dec9bdc0
rdias66 <dias.a@live.com>
2024-01-12 18:18:08 -0300

    home component done, own photo not added yet

--- portfolio/src/components/home/Home.jsx
+++ portfolio/src/components/home/Home.jsx
@@ -1,9 +1,27 @@
 import React from 'react'
 import "./home.css"
+import MyAvatar from '../../assets/avatar-1.svg'
+import HeaderSocials from './HeaderSocials'
+import ScrollDown from './ScrollDown'
+import Shapes from './Shapes'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <section className="home container" id="home">
+      <div className="intro">
+        <img src={MyAvatar} alt="" className="home__img" />
+        <h1 className="home__name">Rodrigo Dias</h1>
+        <span className="home__education">Full Stack Developer</span>
+
+        <HeaderSocials />
+
+        <a href="#contact" className="btn">Hire Me</a>
+
+        <ScrollDown />
+      </div>
+
+      <Shapes />
+    </section>
   )
 }
 
