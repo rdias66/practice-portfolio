a9ef18aa478e7ef2ed5b5ac301c94e2c367b6efa
rdias66 <dias.a@live.com>
2024-01-30 14:25:42 -0300

    services section done

--- portfolio/src/components/services/Services.jsx
+++ portfolio/src/components/services/Services.jsx
@@ -40,6 +40,20 @@ const data = [
 const Services = () => {
   return <section className="services container section" id="services">
     <h2 className="section__title">Services</h2>
+
+    <div className="services__container grid">
+      {data.map(({id, image, title, description}) => {
+        return (
+          <div className="services__card" key={id}>
+              <img src={image} alt="" className="services__img" />
+
+              <h3 className="services__title">{title}</h3>
+
+              <p className="services__description">{description}</p>
+          </div>
+        )
+      })}
+    </div>
   </section>;
 }
 
--- portfolio/src/components/sidebar/Sidebar.jsx
+++ portfolio/src/components/sidebar/Sidebar.jsx
@@ -1,13 +1,10 @@
 import React from 'react'
 import "./sidebar.css"
-import Logo from '../../assets/logo.svg'
+
 
 const Sidebar = () => {
   return (
     <aside className='aside'>
-      <a href="#home" className="nav_logo">
-        <img src={Logo} alt="" />
-      </a>
       <nav className="nav">
         <div className="nav__menu">
           <ul className="nav__list">
