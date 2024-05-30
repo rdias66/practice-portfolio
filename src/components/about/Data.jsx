3f7af019d6aebb84db1c144ae9b3cdab67c25425
Rodrigo Dias <88790315+rdias66@users.noreply.github.com>
2024-05-30 13:10:39 -0300

    Update Data.jsx

--- src/components/about/Data.jsx
+++ src/components/about/Data.jsx
@@ -1,6 +1,6 @@
 const Data = [
     {
-      aboutEN: "Hi! Im Rodrigo Dias, a full stack developer majoring in Analysis and Systems Development at UTFPR-Ponta Grossa. Mostly experienced in backend development, but with full stack junior knowledge and hunger to learn more everyday. ",
+      aboutEN: "This is no longer my main portfolio. This is the one : ",
       aboutPT: "Olá! Eu sou Rodrigo Dias, um desenvolvedor full stack cursando Analise e Desenvolvimento de Sistemas na UTFPR-Ponta grossa. Majoritariamente com experiencia em desenvolvimento backend, porém com conhecimento junior full stack e fome de aprender mais todo dia. ",
     },
   ];
